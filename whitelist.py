2c52997354b3234563c73c8ab792ad20ddc765cf
Stephen Whitlock <stephen@jshwisolutions.com>
2024-04-10 22:19:20 +1000

    chore: update poetry.lock
    Signed-off-by: Stephen Whitlock <stephen@jshwisolutions.com>

--- whitelist.py
+++ whitelist.py
@@ -1,3 +1,3 @@
-fixture_mock_environment  # unused function (tests/conftest.py:17)
-fixture_register_template  # unused function (tests/conftest.py:23)
-fixture_register_templates  # unused function (tests/conftest.py:48)
+fixture_mock_environment  # unused function (tests/conftest.py)
+fixture_register_template  # unused function (tests/conftest.py)
+fixture_register_templates  # unused function (tests/conftest.py)
